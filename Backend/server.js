295464dcf27560a89db8df6bedcdde38280710c5
Lucas Ferraz dos Santos <lucasferrazsantos@hotmail.com>
2026-03-24 15:44:08 -0300

    Backend do CRUD Completo

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,49 @@ app.get('/games', (req, res) => {
             return res.status(500).json({ error: "não achou o banco de dados"});
         }
         res.json(results);
-    })
+    });
+});
+
+app.post('/games', (req, res) => {
+    const { titulo, genero, plataforma, ano_lanc, preco } = req.body;
+
+    const sql = "INSERT INTO games (titulo, genero, plataforma, ano_lanc, preco) VALUES (?, ?, ?, ?, ?)";
+
+    db.query(sql, [titulo, genero, plataforma, ano_lanc, preco], (err, result) => {
+        if (err) {
+            console.error("não inseriu o jogo:", err);
+            return res.status(500).json({ error: "não cadastrou"});
+        }
+        res.status(201).json({ message: "foi cadastrado", id: result.insertId });
+    });
+});
+
+app.delete('/games/:id', (req, res) => {
+    const { id } = req.params;
+    const sql = "DELETE FROM games WHERE id = ?";
+
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            console.error("não deletouo  jogo:", err);
+            return res.status(500).json({ error: "não excluiu o jogo" });
+        }
+        res.json({ message: "jogo foi exluido" });
+    });
+});
+
+app.put('/games/:id', (req, res) => {
+    const { id } = req.params;
+    const { titulo, genero, plataforma, ano_lanc, preco } = req.body;
+
+    const sql = "UPDATE games SET titulo = ?, genero = ?, plataforma = ?, ano_lanc = ?, preco = ? WHERE id = ?";
+
+    db.query(sql, [titulo, genero, plataforma, ano_lanc, preco, id], (err, result) => {
+        if (err) {
+            console.error("não atualizou:", err);
+            return res.status(500).json({ error: "não editou o jogo" });
+        }
+        res.json({ message: "atualizou o jogo"});
+    });
 });
 
 const PORT = process.env.PORT || 3001;
