46a8c81d351baa1df142c13e49a371c69b5b1ccd
Lucas Ferraz dos Santos <lucasferrazsantos@hotmail.com>
2026-03-24 14:35:26 -0300

    criação do Banco de Dados e Integração com o Backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const db = require('./config/db');
 const app = express();
 
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Server ta rodando Nome: Lucas Ferraz")
+app.get('/games', (req, res) => {
+    const sql = "SELECT * FROM games";
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error("não achou os jogos:", err)
+            return res.status(500).json({ error: "não achou o banco de dados"});
+        }
+        res.json(results);
+    })
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>{
     console.log(`Ta rodando na porta ${PORT}`);
 });
